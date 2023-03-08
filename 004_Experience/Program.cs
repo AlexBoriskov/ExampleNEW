5c2c07f42bde9d2f496e137ab9543d7c63654ae2
Alex <sasha-boriskov@yandex.ru>
2023-03-08 19:34:36 +0300

    Среднее арифметическое 4 чисел

---
+++ 004_Experience/Program.cs
@@ -0,0 +1,13 @@
+﻿double a = new Random().Next (0,101);
+Console.WriteLine (a);
+double b = new Random().Next (0,101);
+Console.WriteLine (b);
+double c= new Random ().Next(0,101);
+Console.WriteLine (c);
+double d = new Random ().Next(0,101);
+Console.WriteLine (d);
+
+double sred = (a+b+c+d)/4;
+
+Console.Write ("Среднее арифетическое = ");
+Console.WriteLine (sred);
