d25a27894e031aeb27f55b66624892f38a01ea5d
Alex <sasha-boriskov@yandex.ru>
2023-03-08 19:48:31 +0300

    Добавили поиск минимального числа

--- 004_Experience/Program.cs
+++ 004_Experience/Program.cs
@@ -20,3 +20,12 @@ if (d>max) {max=d;}
 
 Console.Write("Максимальное число= ");
 Console.WriteLine(max);
+
+double min = a;
+
+if (b<min) {min=b;}
+if (c<min) {min=c;}
+if (d<min) {min=d;}
+
+Console.Write("Минимальное число= ");
+Console.WriteLine(min);
